ceecfff7d098f94f6fcb3f4dd81743755650aa87
zds <49744633+zds-s@users.noreply.github.com>
2024-12-02 16:15:37 +0800

    翻译文件

--- .vitepress/src/en/nav.ts
+++ .vitepress/src/en/nav.ts
@@ -1,11 +1,16 @@
 import {DefaultTheme} from "vitepress";
 
 const nav:DefaultTheme.NavItem[] = [
-    { text: 'Guide', link: '/en/guide' },
-    { text: 'Frontend', link: '/en/front' },
-    { text: 'Backend', link: '/en/backend' },
-    { text: 'Frequently Asked Questions', link: '/en/faq' },
-    { text: 'Old Version Documentation', link: 'https://docv2.mineadmin.com' },
+    { text: 'Guide', link: '/en/guide/introduce/mineadmin' },
+    { text: 'Frontend', link: '/en/front/base/concept' },
+    { text: 'Backend', link: '/en/backend/index' },
+    { text: 'Plugins', link: '/en/plugin/index' },
+    { text: 'FAQ', link: '/en/faq/index' },
+    { text: 'More' ,items:[
+        { text: 'Old Version Documentation', link: 'https://docv2.mineadmin.com' },
+        { text: 'Hyperf Component Library', link: 'https://hyperf.fans/' },
+        { text: 'Fantastic-admin Framework', link: 'https://fantastic-admin.hurui.me/' },
+    ]}
 ]
 
 export default nav
