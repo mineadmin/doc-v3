fe6a2a6d15fb10d89a99beff2fabf206abf8b05c
lyan <49744633+zds-s@users.noreply.github.com>
2024-12-02 16:01:28 +0800

    Update nav.ts

--- .vitepress/src/zh/nav.ts
+++ .vitepress/src/zh/nav.ts
@@ -6,7 +6,11 @@ const nav:DefaultTheme.NavItem[] = [
     { text: '后端', link: '/zh/backend/index' },
     { text: '插件',link: "/zh/plugin/index"},
     { text: '常见问题', link: '/zh/faq/index' },
-    { text: '老版本文档', link: 'https://docv2.mineadmin.com' },
+    { text: '更多推荐' ,items:[
+        { text: '老版本文档', link: 'https://docv2.mineadmin.com' },
+        { text: '开箱即用的 Hyperf 组件库', link: 'https://hyperf.fans/' },
+        { text: 'Fantastic-admin前端框架',link: 'https://fantastic-admin.hurui.me/'},
+    ]}
 ]
 
 export default nav
